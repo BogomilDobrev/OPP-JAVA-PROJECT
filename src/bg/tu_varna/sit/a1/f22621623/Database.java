64e45b94cb89d17f5b136b6cda646f443d067356
unknown <bogomildobrev3@gmail.com>
2024-05-04 21:42:08 +0300

    ChangedDbAndAddedMain

--- src/bg/tu_varna/sit/a1/f22621623/Database.java
+++ src/bg/tu_varna/sit/a1/f22621623/Database.java
@@ -1,19 +1,32 @@
 package bg.tu_varna.sit.a1.f22621623;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 public class Database {
-    private HashMap<String, List<Table>> database;
-    public Database() {
-        database = new HashMap<>();
+    private String databaseName;
+    private HashMap<String, Table> databaseTables;
+    public Database(String databaseName) {
+        this.databaseName = databaseName;
+        this.databaseTables = new LinkedHashMap<>();
     }
 
-    public HashMap<String, List<Table>> getDatabase() {
-        return database;
+    public HashMap<String, Table> getDatabaseTables() {
+        return databaseTables;
+    }
+    public void addTable(String tableName, Table table) {
+        if(databaseTables.containsKey(tableName)) {
+            throw new IllegalArgumentException("Table already exists!");
+        }
+        databaseTables.put(tableName, table);
     }
 
-    public void setDatabase(HashMap<String, List<Table>> database) {
-        this.database = database;
+    public String getDatabaseName() {
+        return databaseName;
+    }
+
+    public void setDatabaseName(String databaseName) {
+        this.databaseName = databaseName;
     }
 }
